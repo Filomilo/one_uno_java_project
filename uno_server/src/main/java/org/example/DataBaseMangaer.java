7ab742e5350092e9d6fde7e6d849d25973218e7b
Filomilo <60512955+Filomilo@users.noreply.github.com>
2023-03-06 20:20:43 +0100

    Prepared basic Data Bse connection class and procedure activataion

---
+++ uno_server/src/main/java/org/example/DataBaseMangaer.java
@@ -0,0 +1,233 @@
+package org.example;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DataBaseMangaer {
+
+    private Connection connection;
+    private String dataBasePort;
+    private String dataBaseAdres;
+    private String dataBaseName;
+    private String dataBaseUserName;
+    private String dataBasePass;
+    public void setDataBaseName(String dataBaseName) {
+        this.dataBaseName = dataBaseName;
+    }
+
+    public void setDataBasePort(String dataBasePort) {
+        this.dataBasePort = dataBasePort;
+    }
+
+    public void setDataBaseUserName(String dataBaseUserName) {
+        this.dataBaseUserName = dataBaseUserName;
+    }
+
+    public void setDataBasePass(String dataBasePass) {
+        this.dataBasePass = dataBasePass;
+    }
+
+    public void setDataBaseAdres(String dataBaseAdres) {
+        this.dataBaseAdres = dataBaseAdres;
+    }
+
+    DataBaseMangaer()
+    {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public DataBaseMangaer(String dataBaseAdres,String dataBasePort, String dataBaseName, String dataBaseUserName, String dataBasePass) {
+        this.dataBasePort = dataBasePort;
+        this.dataBaseAdres = dataBaseAdres;
+        this.dataBaseName = dataBaseName;
+        this.dataBaseUserName = dataBaseUserName;
+        this.dataBasePass = dataBasePass;
+    }
+
+    void connectWithDataBase()
+    {
+        try {
+            this.connection= DriverManager.getConnection("jdbc:oracle:thin:@"+this.dataBaseAdres + ":"+ this.dataBasePort +":"+this.dataBaseName , this.dataBaseUserName, this.dataBasePass);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    boolean checkTable() throws SQLException {
+        DatabaseMetaData metadata= this.connection.getMetaData();
+        String[] types={"TABLE"};
+        String[] searchedTablesArray={"PLAYERS","GAMES","CARDS","ACTIVE_CARD_PLACES"};
+        List<String> searchedTables= new ArrayList<String>();
+        searchedTables.addAll(Arrays.asList(searchedTablesArray));
+        ResultSet resultSet=metadata.getTables(null,null,"%", types);
+        while(resultSet.next()) {
+        searchedTables.remove(resultSet.getString("TABLE_NAME"));
+        }
+        return searchedTables.size()==0;
+        }
+
+
+    void createTables()
+    {
+        executeArrayStatements(SqlScripts.CreateTablecSripts);
+    }
+
+    void dropTables()
+    {
+        executeArrayStatements(SqlScripts.DropTablecSripts);
+    }
+
+    void deleteAll()
+    {
+        executeArrayStatements(SqlScripts.DeleteAllScripts);
+    }
+
+    void createViews()
+    {
+        executeArrayStatements(SqlScripts.CreateViewsScripts);
+    }
+
+    void createSeq()
+    {
+        executeArrayStatements(SqlScripts.CreateSequencesScripts);
+    }
+    void dropSeq()
+    {
+        executeArrayStatements(SqlScripts.DropSequencesScripts);
+    }
+
+    void createFunctions()
+    {
+        executeArrayStatements(SqlScripts.CreateFunctions);
+    }
+
+    void createProcedures()
+    {
+        executeArrayStatements(SqlScripts.CreateProceduresScripts);
+    }
+
+    void addPlayer(String nick)
+    {
+        String[] arr={nick};
+        executeProcedure(SqlScripts.AddPlayerScript,arr);
+    }
+
+    void createBaseCards()
+    {
+        executeProcedure(SqlScripts.FillBaseCardsScripts);
+    }
+
+    void createNewGame(String nick)
+    {
+        String[] arr={nick};
+        executeProcedure(SqlScripts.CreateNewGameScripts,arr);
+    }
+
+    void addPlayerToGame(String nick)
+    {
+        String[] arr={nick};
+        executeProcedure(SqlScripts.AddPlayerToGameScript,arr);
+    }
+
+    void drawCard(String nick)
+    {
+        String[] arr={nick};
+        executeProcedure(SqlScripts.DrawCardScript,arr);
+    }
+
+    void preapreDeck()
+    {
+        executeProcedure(SqlScripts.PreapareDeckScript);
+    }
+
+    void dealCards()
+    {
+        executeProcedure(SqlScripts.DealCardScript);
+    }
+
+    void playCard(String nick, int pos)
+    {
+        executeProcedure(SqlScripts.PlayCardScript,nick,pos);
+    }
+
+    void reshuffleDeck()
+    {
+        executeProcedure(SqlScripts.ReshuffleDeck);
+    }
+
+    void surrender(String nick)
+    {
+        String[] arr={nick};
+        executeProcedure(SqlScripts.SurrenderScript,arr);
+    }
+    void setRank(String nick)
+    {
+        String[] arr={nick};
+        executeProcedure(SqlScripts.SetRankScript,arr);
+    }
+
+    void clearGame()
+    {
+        executeProcedure(SqlScripts.ClearGameSrript);
+    }
+
+    private void executeProcedure(String sqlCode)
+    {
+            try{
+                Statement statement = connection.createStatement();
+                statement.execute(sqlCode);
+            } catch (SQLException e) {
+                System.out.println(sqlCode + "--------------didint execute porpely---------------");
+                e.printStackTrace();
+            }
+    }
+
+    private void executeProcedure(String sqlCode, String[] vars)
+    {
+        try{
+            PreparedStatement statement = connection.prepareStatement(sqlCode);
+            for (int i=1;i<=vars.length;i++)
+            {
+                statement.setString(i,vars[i-1]);
+            }
+            statement.executeQuery();
+        } catch (SQLException e) {
+            System.out.println(sqlCode + "--------------didint execute porpely---------------");
+           // e.printStackTrace();
+        }
+    }
+    private void executeProcedure(String sqlCode, String var, int numb )
+    {
+        try{
+            PreparedStatement statement = connection.prepareStatement(sqlCode);
+            statement.setString(1,var);
+            statement.setInt(2,numb);
+            statement.executeQuery();
+        } catch (SQLException e) {
+            System.out.println(sqlCode + "--------------didint execute porpely---------------");
+            // e.printStackTrace();
+        }
+    }
+
+     private void executeArrayStatements(String [] statemnt)
+     {
+         for (String sqlCode: statemnt) {
+             try{
+             Statement statement = connection.createStatement();
+             statement.execute(sqlCode);
+             } catch (SQLException e) {
+                System.out.println(sqlCode + "--------------didint execute porpely---------------");
+                e.printStackTrace();
+             }
+
+         }
+     }
+
+
+}
--- uno_server/src/main/java/org/example/Main.java
+++ uno_server/src/main/java/org/example/Main.java
@@ -1,15 +1,66 @@
 package org.example;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // ServerApp serverApp=new ServerApp();
+        DataBaseMangaer dataBaseMangaer= new DataBaseMangaer();
+        dataBaseMangaer.setDataBaseName("orcl");
+        dataBaseMangaer.setDataBaseAdres("localhost");
+        dataBaseMangaer.setDataBasePort("1521");
+        dataBaseMangaer.setDataBaseUserName("test");
+        dataBaseMangaer.setDataBasePass("test");
 
-        ServerApp serverApp=new ServerApp();
+        dataBaseMangaer.connectWithDataBase();
+        try {
+            System.out.println(  dataBaseMangaer.checkTable());
 
-
-
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        /*
+        dataBaseMangaer.dropTables();
+        dataBaseMangaer.createTables();
+
+
+        dataBaseMangaer.deleteAll();
+        dataBaseMangaer.createViews();
+        dataBaseMangaer.dropSeq();
+        dataBaseMangaer.createSeq();
+        dataBaseMangaer.createFunctions();
+        dataBaseMangaer.createProcedures();
+
+
+        dataBaseMangaer.addPlayer("nick");
+        dataBaseMangaer.addPlayer("player");
+        dataBaseMangaer.addPlayer("Filip");
+        dataBaseMangaer.addPlayer("Bartek");
+
+        dataBaseMangaer.createBaseCards();
+
+
+        dataBaseMangaer.createNewGame("nick");
+        dataBaseMangaer.addPlayerToGame("player");
+        dataBaseMangaer.addPlayerToGame("Filip");
+        dataBaseMangaer.preapreDeck();
+        dataBaseMangaer.dealCards();
+
+
+        dataBaseMangaer.playCard("Filip",2);
+
+        dataBaseMangaer.drawCard("Filip");
+        dataBaseMangaer.drawCard("Filip");
+        dataBaseMangaer.drawCard("Filip");
+        dataBaseMangaer.drawCard("Filip");
+        dataBaseMangaer.drawCard("Filip");
+        dataBaseMangaer.drawCard("Filip");
+        dataBaseMangaer.drawCard("Filip");
+         */
+
+
+    }
         // System.out.println("Hello world from server!"
     }
